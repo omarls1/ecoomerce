05ae20554fb00da0ad9a122f776e06684c960dcd
Omar Lammas <omarlammas@gmail.com>
2024-03-10 11:21:42 +0000

    add bootstrap and fontawesome

---
+++ components/header.vue
@@ -0,0 +1,9 @@
+<template>
+  <p class="text-primary">gggggg</p>
+  <div></div>
+  <nav>
+    <div class="container">
+      <NuxtLink to="#"> </NuxtLink>
+    </div>
+  </nav>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <p class="text-primary">gggggg</p>
+  <div>
+<font-awesome-icon :icon="['fas', 'house']" />
+  </div>
+</template>
